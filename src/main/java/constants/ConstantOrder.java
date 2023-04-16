552fa838e6bef4609ebec682bde539c5645cec0d
Aleksandr Bondarev <alexandr.s.bondarev@yandex.ru>
2023-04-17 04:02:22 +0600

    add LoginCourierTest

---
+++ src/main/java/constants/ConstantOrder.java
@@ -0,0 +1,12 @@
+package constants;
+
+public class ConstantOrder {
+    public static final String FIRST_NAME_ORDERS = "Александр";
+    public static final String lAST_NAME_ORDERS = "Бондарев";
+    public static final String ADDRESS = "Сокольническая Площадь 4А";
+    public static final String METRO_STATION = "Сокольники";
+    public static final String PHONE =  "+79136207921";
+    public static final Integer RENT_TIME = 6 ;
+    public static final String DELIVERY_DATE = "2023-06-06";
+    public static final String COMMENT = "ТЕСТ ТЕСТ АВТОТЕСТ";
+}
---
+++ src/test/java/CreateOrderTest.java
@@ -0,0 +1,51 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import order.Order;
+import order.OrderApi;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+import static constants.ConstantEndpoints.MAIN_PAGE;
+import static constants.ConstantOrder.*;
+import static org.apache.http.HttpStatus.SC_CREATED;
+import static org.hamcrest.Matchers.notNullValue;
+
+@RunWith(Parameterized.class)
+public class CreateOrderTest{
+        private final List<String> color;
+        OrderApi orderApi = new OrderApi();
+
+        public CreateOrderTest(List<String> color) {
+            this.color = color;
+        }
+
+        @Parameterized.Parameters
+        public static Object[][] checkCreateOrderWithChoiceColor() {
+            return new Object[][]{
+                    {List.of("GREY")},
+                    {List.of("BLACK")},
+                    {List.of("GREY", "BLACK")},
+                    {List.of()},
+            };
+        }
+
+        @Before
+        public void setUp() {
+            RestAssured.baseURI = MAIN_PAGE;
+        }
+
+        @Test
+        @DisplayName("Создание заказа с выбором цвета")
+        @Description("Проверка, что можно создать заказ на: серый цвет, черный цвет, выбор двух цветов, без выбора цвета")
+        public void paramCreateOrderTest() {
+            Order order = new Order(FIRST_NAME_ORDERS, lAST_NAME_ORDERS, ADDRESS, METRO_STATION, PHONE, RENT_TIME, DELIVERY_DATE, COMMENT, color);
+            ValidatableResponse response = orderApi.createOrder(order);
+            response.assertThat().statusCode(SC_CREATED).and().body("track", notNullValue());
+        }
+}
